7867de43b1587932bb1fc383e473891f5f4ce800
wilhamsousa <wilhamsousa@gmail.com>
2024-12-22 14:52:20 -0300

    swagger ajust

--- Uex.ContactBook.Api/Extensions/Services/SwaggerConfiguration.cs
+++ Uex.ContactBook.Api/Extensions/Services/SwaggerConfiguration.cs
@@ -32,6 +32,33 @@ namespace Uex.ContactBook.Api.Extensions.Services
                 xmlFile = "Uex.ContactBook.Domain.xml";
                 xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
                 x.IncludeXmlComments(xmlPath);
+
+                x.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                x.AddSecurityRequirement(new OpenApiSecurityRequirement(){
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header,
+
+                        },
+                        new List<string>()
+                    }
+                });
             });
             services.AddSwaggerGenNewtonsoftSupport();
         }
